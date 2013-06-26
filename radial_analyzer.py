ce7326b11e89dcd78b1525a5a4e4514a35f8e49d
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2013-06-26 02:35:01 -0700

    Add polynomial constructor and degree

--- radial_analyzer.py
+++ radial_analyzer.py
@@ -1,7 +1,24 @@
 class Polynomial(object):
     """Polynomials, immutable, with floating point coefficients"""
 
+    def __init__(self, c = 0, n = 0):
+        self.__coeffs = [0 for i in range(n)] + [c]
+        self.__standardize()
+
+    def __standardize(self):
+        while self.degree >= 0 and self.__coeffs[self.degree] == 0:
+            self.__coeffs.pop()
+
+    @property
+    def degree(self):
+        return len(self.__coeffs) - 1
+
     def __eq__(self, other):
+        if self.degree != other.degree:
+            return False
+        for i in range(self.degree + 1):
+            if self.__coeffs[i] != other.__coeffs[i]:
+                return False
         return True
     def __ne__(self, other):
         return not (self == other)
--- radial_analyzer_test.py
+++ radial_analyzer_test.py
@@ -4,7 +4,51 @@ import unittest
 class TestPolynomial(unittest.TestCase):
 
     def testPolynomial(self):
-        self.assertEqual(Polynomial(), Polynomial())
+        z = Polynomial()
+        n = Polynomial(1)
+        x = Polynomial(1, 1)
+
+        self.assertEqual(z, z)
+        self.assertEqual(n, n)
+        self.assertEqual(x, x)
+        self.assertNotEqual(z, n)
+        self.assertNotEqual(z, x)
+        self.assertNotEqual(n, x)
+
+        self.assertEqual(z, Polynomial(0))
+        self.assertEqual(z, Polynomial(0, 0))
+        self.assertEqual(n, Polynomial(1, 0))
+
+        self.assertEqual(z, Polynomial(0, 3))
+        self.assertNotEqual(n, Polynomial(0, 3))
+        self.assertNotEqual(x, Polynomial(0, 3))
+
+        self.assertNotEqual(z, Polynomial(3, 0))
+        self.assertNotEqual(n, Polynomial(3, 0))
+        self.assertNotEqual(x, Polynomial(3, 0))
+
+        self.assertNotEqual(z, Polynomial(3, 1))
+        self.assertNotEqual(n, Polynomial(3, 1))
+        self.assertNotEqual(x, Polynomial(3, 1))
+
+        self.assertNotEqual(z, Polynomial(1, 3))
+        self.assertNotEqual(n, Polynomial(1, 3))
+        self.assertNotEqual(x, Polynomial(1, 3))
+
+        self.assertEqual(Polynomial(6), Polynomial(6, 0))
+        self.assertNotEqual(Polynomial(6), Polynomial(1, 1))
+        self.assertNotEqual(Polynomial(6), Polynomial(1, 6))
+        self.assertNotEqual(Polynomial(6), Polynomial(6, 1))
+        self.assertNotEqual(Polynomial(6), Polynomial(6, 6))
+
+        self.assertEqual(Polynomial().degree, -1)
+        self.assertEqual(Polynomial(0).degree, -1)
+        self.assertEqual(Polynomial(6).degree, 0)
+        self.assertEqual(Polynomial(4, 0).degree, 0)
+        self.assertEqual(Polynomial(3, 1).degree, 1)
+        self.assertEqual(Polynomial(1, 3).degree, 3)
+        self.assertEqual(Polynomial(0, 1).degree, -1)
+        self.assertEqual(Polynomial(0, 5).degree, -1)
 
 if __name__ == '__main__':
     unittest.main()
