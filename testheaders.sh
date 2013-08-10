58ea5e4839ccd7b177eca408189882161d9ed2d5
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2013-08-10 14:15:57 -0700

    testheaders.sh cleans up after itself better

--- testheaders.sh
+++ testheaders.sh
@@ -7,11 +7,13 @@ else
     headers="*.hh"
 fi
 
+crap='testheader.cc testheader.o .testheader.d'
+
 for header in $headers
 do
-    rm -f testheader.cc testheader.o
+    rm -f $crap
     echo '#include "'$header'"' >> testheader.cc
     echo 'Compiling '$header
-    make testheader.o || { rm -f testheader.cc testheader.o; exit 1; }
+    make testheader.o || { rm -f $crap; echo Failed on $header; exit 1; }
 done
-rm -f testheader.cc testheader.o
+rm -f $crap
